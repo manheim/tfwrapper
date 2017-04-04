fd17ee60de0b85f30841406092c8234bb1aaa944
Jason Antman <jason@jasonantman.com>
2017-04-04 13:51:44 -0400

    add note in version.rb about updating Installation section of README.md

--- lib/tfwrapper/version.rb
+++ lib/tfwrapper/version.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module TFWrapper
-  # version of the Gem/module; used in the gemspec and in messages
+  # version of the Gem/module; used in the gemspec and in messages.
+  # NOTE: When updating this, also update the version in the "Installation"
+  # section of README.md
   VERSION = '0.2.0'
 end
