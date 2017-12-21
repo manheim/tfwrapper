cad8c88351e03eefe316a70f23cb869eb1e312b8
Jason Antman <jason@jasonantman.com>
2017-12-21 14:20:11 -0500

    rubocop fix

--- spec/acceptance/acceptance_spec.rb
+++ spec/acceptance/acceptance_spec.rb
@@ -8,11 +8,11 @@ require 'json'
 TF_VERSION = desired_tf_version
 if Gem::Version.new(TF_VERSION) >= Gem::Version.new('0.10.0')
   APPLY_CMD = 'terraform apply -auto-approve'
-  if Gem::Version.new(TF_VERSION) >= Gem::Version.new('0.11.0')
-    EXPECTED_SERIAL = 1
-  else
-    EXPECTED_SERIAL = 2
-  end
+  EXPECTED_SERIAL = if Gem::Version.new(TF_VERSION) < Gem::Version.new('0.11.0')
+                      2
+                    else
+                      1
+                    end
 else
   EXPECTED_SERIAL = 1
   APPLY_CMD = 'terraform apply'
